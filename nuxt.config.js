87544101f84a0ca7f67c567e415a0b710529d012
saqtaka <50040766+saqtaka@users.noreply.github.com>
2021-05-22 00:40:48 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
+  ssr: 'false',
+  target: 'static',
   docs: {
-    primaryColor: '#E24F55'
+    primaryColor: '#00CD81'
   }
 })
