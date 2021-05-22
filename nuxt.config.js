9c3e4907c465c8daffab6749574c6a253fce0b65
saqtaka <50040766+saqtaka@users.noreply.github.com>
2021-05-22 13:55:07 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,5 +4,19 @@ export default theme({
   target: 'static',
   docs: {
     primaryColor: '#00CD81'
-  }
+  },
+  i18n: {
+    locales: () => [{
+      code: 'ja',
+      iso: 'ja-JP',
+      file: 'ja-JP.js',
+      name: '日本語'
+    }, {
+      code: 'en',
+      iso: 'en-US',
+      file: 'en-US.js',
+      name: 'English'
+    }],
+    defaultLocale: 'ja'
+  },
 })
